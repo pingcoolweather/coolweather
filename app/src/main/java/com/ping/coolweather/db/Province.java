c0bf806559f3655d51283dd8c81e19585dee0ef9
unknown <you@example.com>
2018-05-18 16:21:16 +0800

    加入创建数据库和表的配置

---
+++ app/src/main/java/com/ping/coolweather/db/Province.java
@@ -0,0 +1,36 @@
+package com.ping.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport{
+
+    private int id;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private String provinceName;
+    private int provinceCode;
+
+}
